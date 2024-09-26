d56efb6a382ca2a32a2a078d2f080448a4c03f95
tien.blt <buitien747@gmail.com>
2024-09-26 15:54:06 +0700

    add register controller

---
+++ src/main/java/com/tienblt/project/controllers/RegisterController.java
@@ -0,0 +1,86 @@
+package com.tienblt.project.controllers;
+
+import java.io.IOException;
+
+import com.tienblt.project.services.IUserService;
+import com.tienblt.project.services.impl.UserServiceImpl;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet(urlPatterns = "/register")
+public class RegisterController extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7194911657498807595L;
+	public static final String REGISTER = "/views/register.jsp";
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// when create = false, if there is no session associated with client, it return
+		// null
+		HttpSession session = req.getSession(false);
+		if (session != null && session.getAttribute("username") != null) {
+			resp.sendRedirect(req.getContextPath() + "/waiting");
+			return;
+		}
+		// Check cookie
+		Cookie[] cookies = req.getCookies();
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				if (cookie.getName().equals("username")) {
+					session = req.getSession(true);
+					session.setAttribute("username", cookie.getValue());
+					resp.sendRedirect(req.getContextPath() + "/waiting");
+					return;
+				}
+			}
+		}
+		req.getRequestDispatcher(REGISTER).forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		resp.setCharacterEncoding("UTF-8");
+		req.setCharacterEncoding("UTF-8");
+
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		String email = req.getParameter("email");
+		String fullname = req.getParameter("fullname");
+		String phone = req.getParameter("phone");
+		IUserService service = new UserServiceImpl();
+		String alertMsg = "";
+		if (service.checkExistEmail(email)) {
+			alertMsg = "Email đã tồn tại!";
+			req.setAttribute("alert", alertMsg);
+			req.getRequestDispatcher(REGISTER).forward(req, resp);
+			return;
+		}
+		if (service.checkExistUserName(username)) {
+			alertMsg = "Tài khoản đã tồn tại!";
+			req.setAttribute("alert", alertMsg);
+			req.getRequestDispatcher(REGISTER).forward(req, resp);
+			return;
+		}
+		boolean isSuccess = service.register(username, email, password, fullname, phone);
+		if (isSuccess) {
+			req.setAttribute("alert", alertMsg);
+			resp.sendRedirect(req.getContextPath() + "/login");
+		} else {
+			alertMsg = "System error!";
+			req.setAttribute("alert", alertMsg);
+			req.getRequestDispatcher(REGISTER).forward(req, resp);
+		}
+
+	}
+
+}
