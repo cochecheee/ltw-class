b7c6556a39ff68c601ce16fe225814afa82ade24
tien.blt <buitien747@gmail.com>
2024-10-01 12:31:47 +0700

    add download controller

---
+++ src/main/java/com/tienblt/project/controllers/DownloadFileController.java
@@ -0,0 +1,36 @@
+package com.tienblt.project.controllers;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+
+import com.tienblt.project.utils.Constants;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = { "/image" })
+public class DownloadFileController extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8347627367542401776L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		String fileName = req.getParameter("fname");
+		File file = new File(Constants.DIR + "/" + fileName);
+		resp.setContentType("image/jpeg");
+		if (file.exists()) {
+			IOUtils.copy(new FileInputStream(file), resp.getOutputStream());
+		}
+
+	}
+}
